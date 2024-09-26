b118efe7a01e7733d83949373be56bd9dfea923e
TheB05 <branderbaez02@gmail.com>
2024-09-26 18:40:02 -0400

    Changes to the files: NewRegist and Report.

--- Models/Report.cs
+++ Models/Report.cs
@@ -1,12 +1,21 @@
-
+using System;
+using System.ComponentModel.DataAnnotations;
 
 class Reporte{
 
     public string ID = Guid.NewGuid().ToString();
+
     public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required (ErrorMessage = "Necesita introducir una descripción.")]
+    [MinLength(10, ErrorMessage = "Debe introducir una descripción con al menos 5 caracteres para que sea válida.")]
     public string Descripcion { get; set; } = "";
+
     public double CostoEstimado { get; set; } = 0;
+
     public int Muertos { get; set; } = 0;
+
     public int Heridos { get; set; } = 0;
+
     public int VehiculosInvolucrados { get; set; } = 0;
 }
--- obj/Debug/net8.0/Tarea3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tarea3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a691ff0fbc313a9d5c2f4b1f7da26a89cd8b03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6d84a9c5af75056f6a836cfa5108d2bb090a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
